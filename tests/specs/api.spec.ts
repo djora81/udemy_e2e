8ad6a7c6f6c8e109dba17cba85ae6398a3fbe7f4
Alex Udemy <djora81+alex_udemy@gmail.com>
2026-03-24 15:11:56 +0200

    added POST test

--- tests/specs/api.spec.ts
+++ tests/specs/api.spec.ts
@@ -15,6 +15,31 @@ test.describe('API Tests', () => {
     expect(responseBody).toHaveProperty('title');
   });
 
-});
+  test('POST api test', async ({ request }) => {
+    const response = await request.post('https://jsonplaceholder.typicode.com/posts', {
+      data: {
+        title: 'b ready crew',
+        body: 'cafe bar anna',
+        userId: 191,
+        qwerty: 2345,
+        bookTitle: 'Book Title',
+        author: 'John Doe',
+      },
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
 
+    });
+    expect(response.status()).toBe(201);
+    const responseBody = await response.json();
+    console.log(responseBody);
+    expect(responseBody).toHaveProperty('title', 'b ready crew');
+    expect(responseBody).toHaveProperty('body', 'cafe bar anna');
+    expect(responseBody).toHaveProperty('userId', 191);
+    expect(responseBody).toHaveProperty('qwerty', 2345);
+    expect(responseBody).toHaveProperty('bookTitle', 'Book Title');
+    expect(responseBody).toHaveProperty('author', 'John Doe');
+  });
+
+});
   
