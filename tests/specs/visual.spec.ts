4bdfce637e0569ff1387d4e881de78429d922d27
Alex Udemy <djora81+alex_udemy@gmail.com>
2026-04-04 09:42:40 +0300

    added visual catalog page spec

--- tests/specs/visual.spec.ts
+++ tests/specs/visual.spec.ts
@@ -1,10 +1,20 @@
 import {test, expect} from '@playwright/test';
 
-test('Verify product images are displayed', async ({ page }) => {
+test('Verify login page image', async ({ page }) => {
   await page.goto('/login');
   await page.waitForLoadState('networkidle');
 
   await expect(page).toHaveScreenshot('login-page.png', {
+     fullPage: true,
+     animations: 'disabled',
+    });
+});
+
+test('Verify catalog page image', async ({ page }) => {
+  await page.goto('/');
+  await page.waitForLoadState('networkidle');
+
+  await expect(page).toHaveScreenshot('catalog-page.png', {
      fullPage: true,
      animations: 'disabled',
     });
